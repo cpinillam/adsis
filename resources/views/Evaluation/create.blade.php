11fbb75fa375bf676be82c468fd330d3e6d95b9c
Nellay <nellay2020@gmail.com>
2019-12-02 17:02:25 +0100

    problemas con el js

--- resources/views/Evaluation/create.blade.php
+++ resources/views/Evaluation/create.blade.php
@@ -11,29 +11,56 @@
     <form id="create" class='formular' action='/evaluation' method='POST'>
         @csrf
         <label>Meteo personal:</label>
-        <input class="campos" type="value" name="meteo" value="{{$evaluation->meteo}}">
+        <input type="range" name="learning" min="0" max="5" id="h" value="{{$evaluation->meteo}}">
         <br>
         <label>Idioma:</label>
-        <input class="campos" type="number" name="language" value="{{$evaluation->language}}">
+        <input type="range" name="learning" min="0" max="5" id="myRange" class="slider"value="{{$evaluation->language}}">
         <br>
         <label>Actitud:</label>
-        <input class="campos" type="number" name="attitude" value="{{$evaluation->attitude}}">
+        <input type="range" name="learning" min="0" max="5" value="{{$evaluation->attitude}}">
         <br>
         <label>Participación:</label>
-        <input class="campos" type="number" name="participation" value="{{$evaluation->participation}}">
+        <input type="range" name="learning" min="0" max="5" value="{{$evaluation->participation}}">
         <br>
         <label>Aprendizaje:</label>
-        <input class="campos" type="number" name="learning" value="{{$evaluation->learning}}">
+        <input type="range" name="learning" min="0" max="5" value="{{$evaluation->learning}}">
         <br>
         <label>Colaboración:</label>
-        <input class="campos" type="number" name="collaboration" value="{{$evaluation->collaboration}}">
+        <input type="range" name="learning" min="0" max="5" value="{{$evaluation->collaboration}}">
         <br>
         <input class="campos" type="hidden" name="user_id" value="{{$evaluation->user_id}}">
         <input type="submit" class="boton" value="Enviar">
+
         <br>
     </form>
+
 </div>
 @endsection
 
 
+@section('scripts')
 
+<script>
+    var slider = document.getElementById("myRange");
+    var output = document.getElementsByTagName("class");
+    slider.oninput = function() {
+    output.innerHTML = this.class;
+}
+  //  var slider = document.getElementsById("h");
+   // var slider = document.getElementsByTagName("value");
+
+    //output.innerHTML = slider.value;
+   // slider.oninput =function(){
+   //     output.innerHTML = this.value;
+   // }
+
+//slider.oninput = function() {
+//	$('.count').text(this.value)({
+//		'left': this.value + '%',
+//		'transform': 'translateX(-' + this.value + '%)'
+//	});
+  //  ('width', this.value + '%');
+    //return value}
+
+</script>
+@endsection
