0fb8ada944366d8ee51358fab0432e10e046ce94
Nellay <nellay2020@gmail.com>
2019-12-05 16:29:07 +0100

    el cerebro me iba explotando

--- resources/js/slider.js
+++ resources/js/slider.js
@@ -1,10 +1,9 @@
 var elem = document.querySelector('input[type="range"]');
-    console.log(12)
-    var rangeValue = function(){
-    var newValue = elem.value;
-    var target = document.querySelector('.value');
+console.log(12)
+var rangeValue = function(){
+  var newValue = elem.value;
+  var target = document.querySelector('.value');
   target.innerHTML = newValue;
 }
 
 elem.addEventListener("input", rangeValue);
-
